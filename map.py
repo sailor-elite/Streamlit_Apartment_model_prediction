0d11110b25e4d33c5368b501536a7103bb0596a1
qq <jakub.j.bednarczyk@gmail.com>
2025-07-16 15:22:01 +0200

    Fixed on-click map rerendering.

--- map.py
+++ map.py
@@ -7,27 +7,33 @@ import streamlit as st
 
 def display_map():
     """
-    Displays Streamlit Folium map and registers latitude and longitude based on user's activity.
-    Map uses container width.
-    Args:
+    Displays a Streamlit Folium map and updates session_state with coordinates
+    based on user's click. Prevents map from re-rendering unnecessarily.
+    """
+    if "initial_lat" not in st.session_state:
+        st.session_state.initial_lat = 52.2
+    if "initial_lon" not in st.session_state:
+        st.session_state.initial_lon = 19.21
 
+    if "clicked_lat" not in st.session_state:
+        st.session_state.clicked_lat = None
+    if "clicked_lon" not in st.session_state:
+        st.session_state.clicked_lon = None
 
-    Returns:
-        st.session_state.lat : st.session_state element with latitude based on user click
-        st.session_state.lon : st.session_state element with longitude based on user click
-     """
-
-    lat = st.session_state.get("lat", 52.2)
-    lon = st.session_state.get("lon", 19.21)
     make_map_responsive = """
      <style>
      [title~="st.iframe"] { width: 100%}
      </style>
     """
     st.markdown(make_map_responsive, unsafe_allow_html=True)
-    m = folium.Map(location=[lat, lon], zoom_start=6)
+
+    m = folium.Map(location=[st.session_state.initial_lat, st.session_state.initial_lon], zoom_start=6)
     m.add_child(folium.LatLngPopup())
-    output = st_folium(m, use_container_width=True)
+
+    output = st_folium(m, use_container_width=True, key="static_map")
+
     if output and output["last_clicked"]:
-        st.session_state.lat = output["last_clicked"]["lat"]
-        st.session_state.lon = output["last_clicked"]["lng"]
+        st.session_state.clicked_lat = output["last_clicked"]["lat"]
+        st.session_state.clicked_lon = output["last_clicked"]["lng"]
+
+    return st.session_state.clicked_lat, st.session_state.clicked_lon
